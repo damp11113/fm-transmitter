324cfbcf6efe2f1a831c371a2433e42cc99d6213
damp11113 <damp51252@gmail.com>
2023-08-09 22:33:46 +0700

    new update

---
+++ code/main controller/rotaryencodertest.py
@@ -0,0 +1,40 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import rotaryio
+import board
+import digitalio
+import usb_hid
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+
+button = digitalio.DigitalInOut(board.GP28)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+
+encoder = rotaryio.IncrementalEncoder(board.GP26, board.GP27)
+
+cc = ConsumerControl(usb_hid.devices)
+
+button_state = None
+last_position = encoder.position
+
+while True:
+    current_position = encoder.position
+    position_change = current_position - last_position
+    if position_change > 0:
+        for _ in range(position_change):
+            cc.send(ConsumerControlCode.VOLUME_INCREMENT)
+        print(current_position)
+    elif position_change < 0:
+        for _ in range(-position_change):
+            cc.send(ConsumerControlCode.VOLUME_DECREMENT)
+        print(current_position)
+    last_position = current_position
+    if not button.value and button_state is None:
+        button_state = "pressed"
+    if button.value and button_state == "pressed":
+        print("Button pressed.")
+        cc.send(ConsumerControlCode.PLAY_PAUSE)
+        button_state = None
---
+++ hardware/Fritzing/readme.txt
@@ -0,0 +1,1 @@
+please install parts in to your Fritzing before open "fm transmitter.fzz"
